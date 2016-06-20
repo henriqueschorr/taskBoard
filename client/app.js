1dbba0459ba55c671d82dd85fba86087c07b4b7b
Henrique <henrique.schorr@gmail.com>
2016-06-19 22:54:54 -0300

    Almost Finished - Date Format Missing

--- client/app.js
+++ client/app.js
@@ -1,8 +1,12 @@
-var myApp = angular.module('myApp', ['ngRoute'])
+var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
     $routeProvider.when('/', {
         controller: 'TasksController',
         templateUrl: 'views/tasks.html'
     })
+        .when('/developer', {
+            controller: 'TasksController',
+            templateUrl: 'views/developer.fragment.html'
+        })
 });
--- client/controllers/tasks.js
+++ client/controllers/tasks.js
@@ -6,18 +6,53 @@ var myApp = angular.module('myApp');
 myApp.controller('TasksController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
     console.log('TasksController loaded');
 
+    $(document).ready(function () {
+        $('#createTask').on('shown.bs.modal', function () {
+                $('#titleCreate').focus();
+            })
+            .on('hide.bs.modal', function () {
+                $scope.task = null;
+            });
+
+        $('#editTask').on('shown.bs.modal', function (event) {
+            var taskId = $(event.relatedTarget).context.firstElementChild.textContent;
+            $http.get('/api/tasks/' + taskId).success(function (response) {
+                $scope.task = response;
+                $('#descriptionToDo').focus();
+                $('#createdToDo').val(moment().format('D, MM, YYYY'));
+            });
+        });
+
+        $('#editDoingTask').on('shown.bs.modal', function (event) {
+            var taskId = $(event.relatedTarget).context.firstElementChild.textContent;
+
+            $http.get('/api/tasks/' + taskId).success(function (response) {
+                $scope.task = response;
+                $('#effortDoing').focus();
+            });
+        });
+
+        $('#displayTaskDone').on('shown.bs.modal', function (event) {
+            var taskId = $(event.relatedTarget).context.firstElementChild.textContent;
+
+            $http.get('/api/tasks/' + taskId).success(function (response) {
+                $scope.task = response;
+            });
+        });
+    });
+
     $scope.loadTasks = function () {
         $http.get('/api/tasks/todo').success(function (response) {
             $scope.tasksTodo = response;
-        })
+        });
 
         $http.get('/api/tasks/doing').success(function (response) {
             $scope.tasksDoing = response;
-        })
+        });
 
         $http.get('/api/tasks/done').success(function (response) {
             $scope.tasksDone = response;
-        })
+        });
     };
 
     $scope.addTask = function () {
@@ -36,26 +71,25 @@ myApp.controller('TasksController', ['$scope', '$http', '$location', '$routePara
         })
     };
 
-    $scope.doingTask = function () {
-        var id = $routeParams.id;
-
-        $http.get('/api/tasks/' + id).success(function (response) {
-            var task = response;
-
-            task.status = "Doing";
-            task.developer = "Henrique Schorr";
-
-            $http.put('/api/tasks/' + task._id, task).success(function (response) {
-                $scope.loadTasks();
-            })
-        });
+    $scope.updateTask = function () {
+        $http.put('/api/tasks/' + $scope.task._id, $scope.task).success(function (response) {
+            $scope.loadTasks();
+        })
     };
 
-    $scope.doneTask = function (task) {
-        task.status = "Done";
-        task.finishedOn = Date.now();
+    $scope.doingTask = function () {
+        $scope.task.status = "Doing";
 
-        $http.put('/api/tasks/' + task._id, task).success(function (response) {
+        $http.put('/api/tasks/' + $scope.task._id, $scope.task).success(function (response) {
+            $scope.loadTasks();
+        })
+    };
+
+    $scope.taskDone = function () {
+        $scope.task.status = "Done";
+        $scope.task.finishedOn = Date.now();
+
+        $http.put('/api/tasks/' + $scope.task._id, $scope.task).success(function (response) {
             $scope.loadTasks();
         })
     };
